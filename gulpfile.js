e9852ccdf2c1b4d47a6fe81c24fcd72f8f06a4e1
stigsanek <stigsanek@yandex.ru>
2018-09-14 11:37:49 +0400

    Настаривает сборку проекта в prod

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,62 @@
 var gulp = require("gulp");
-
 var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var del = require("del");
+var run = require("run-sequence");
 
-var browserSync = require("browser-sync");
 
-gulp.task("sass", function() {
+gulp.task("style", function() {
   return gulp.src("src/sass/style.scss")
     .pipe(sass())
-    .pipe(gulp.dest("src/css"))
-    .pipe(browserSync.reload({
-      stream: true
-  }));
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style-min.css"))
+    .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("watch", ["browserSync", "sass"], function() {
-  gulp.watch("src/sass/**/*.scss", ["sass"]);
-  gulp.watch("src/*.html", browserSync.reload);
-  gulp.watch("src/js/**/*.js", browserSync.reload);
+gulp.task("clean", function() {
+  return del("build");
 });
 
-gulp.task("browserSync", function() {
-  browserSync({
-    server: {
-      baseDir: "src"
-    }
-  });
+gulp.task("copy", function() {
+  return gulp.src([
+    "src/fonts/**/*.{woff,woff2}",
+    "src/img/**",
+    "src/js/**",
+    "src/*.html"
+  ], {
+    base: "src"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function(done) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    done
+  );
+});
+
+gulp.task("serve", function() {
+    server.init({
+      server: "build/"
+    });
+
+  gulp.watch("src/sass/**/*.scss", ["style"])
+    .on("change", server.reload);
+  gulp.watch("src/*.html", ["build"])
+    .on("change", server.reload);
+  gulp.watch("src/img/**", ["build"])
+    .on("change", server.reload);
+  gulp.watch("src/js/**", ["build"])
+    .on("change", server.reload);
 });
